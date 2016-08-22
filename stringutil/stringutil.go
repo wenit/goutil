43a71178d4f90e4491610979ef397c622d7de099
wenit <435488988@qq.com>
2016-08-22 21:19:24 +0800

    优化字符串工具类

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -21,15 +21,19 @@ var ALL_CHAR_SEQ = []string{
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
 	"V", "W", "X", "Y", "Z"}
 
-//@Title 获取UUID
-//@Description 获取UUID
-//
+/**
+@Title 获取UUID
+@Description 获取UUID
+@return	string	uuid
+*/
 func UUID() string {
 	return uuid.NewV4().String()
 }
 
 /**
 生成指定长度的随机字符串，字符串为数字+大小写字母
+@param	length	长度
+@return	string	随机字符串
  */
 func RandString(length int) string {
 	b := bytes.Buffer{}
@@ -44,13 +48,15 @@ func RandString(length int) string {
 
 /**
 生成6位短信验证码
+@return	string	6位长度的数字
  */
 func SmsCode() string {
 	return RandNumber6()
 }
 
 /**
-生成4图形验证码，数字+大写字母
+生成4位图形验证码，数字+大写字母
+@return	string	4位长度短信验证码
  */
 func ImageCode() string {
 	b := bytes.Buffer{}
@@ -65,7 +71,9 @@ func ImageCode() string {
 
 
 /**
-生成指定长度的随机字符串
+生成指定长度的随机数字字符串
+@param	length	长度
+@return	string	随机数字字符串
  */
 func RandNumber(length int) string {
 	b := bytes.Buffer{}
@@ -78,28 +86,50 @@ func RandNumber(length int) string {
 	return b.String()
 }
 
+/**
+生成6位长度的随机字符串
+@return	string	6位长度随机字符串
+ */
 func RandString6() string {
 	return RandString(6)
 }
 
+/**
+生成4位长度的随机字符串
+@return	string	4位长度随机字符串
+ */
 func RandString4() string {
 	return RandString(4)
 }
 
+/**
+生成4位长度的随机数字字符串
+@return	string	4位随机数字字符串
+ */
 func RandNumber4() string {
 	return RandNumber(4)
 }
 
+/**
+生成6位长度的随机数字字符串
+@return	string	6位随机数字字符串
+ */
 func RandNumber6() string {
 	return RandNumber(6)
 }
 
-func NextInt(len int) int {
+/**
+随机生成指定范围内数字
+@param	length	长度
+@return	int	指定范围内的数字
+ */
+func NextInt(length int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	time.Sleep(time.Nanosecond)
 	//rand.Seed(time.Now().UnixNano())
-	return r.Intn(len)
+	return r.Intn(length)
 }
+
 
 
 
