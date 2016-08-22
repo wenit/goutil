f49282391c77f1e0ac218c88446ff70b06e6396a
wenit <435488988@qq.com>
2016-08-22 18:51:30 +0800

    优化日期工具类

--- dateutil/dateutil.go
+++ dateutil/dateutil.go
@@ -4,9 +4,21 @@ import (
 	"time"
 	"fmt"
 )
-
+//模板格式：2006-01-02
 const DATE_FORMAT_LAYOUT = "2006-01-02"
+//模板格式：20060102
+const DATE_FORMAT_LAYOUT1 = "20060102"
+//模板格式：2006/01/02
+const DATE_FORMAT_LAYOUT2 = "2006/01/02"
+//模板格式：2006/1/2
+const DATE_FORMAT_LAYOUT3 = "2006/1/2"
+//模板格式：2006年1月2日，中文日期
+const DATE_FORMAT_LAYOUT_CN = "2006年1月2日"
+//模板格式：15:04:05
 const TIME_FORMAT_LAYOUT = "15:04:05"
+//模板格式：150405
+const TIME_FORMAT_LAYOUT1 = "150405"
+//模板格式：2006-01-02 15:04:05
 const DATE_TIME_FORMAT_LAYOUT = "2006-01-02 15:04:05"
 
 /**
@@ -92,4 +104,49 @@ func ParseByLayout(value string, layout string) time.Time {
 		fmt.Println(err)
 	}
 	return t
+}
+
+/**
+将特定日期按指定格式进行转换
+@param	t	待转换的时间类型
+@param	layout	转换模板
+@return	Time	时间字符串
+ */
+func FormatDate(t time.Time, layout string) string {
+	tm := time.Unix(t.Unix(), 0)
+	_date := tm.Format(layout)
+	return _date
+}
+
+/**
+日期格式转换
+@param	value	待转换的时间字符串
+@param	src	源转换模板
+@param	dest	目标转换模板
+@return	string	目标日期字符串
+ */
+func FormatConvert(date string, src string, dest string) string {
+	return FormatDate(ParseByLayout(date, src), dest)
+}
+
+/**
+获取当前日期几天前的日期
+日期模板：2006-01-02
+@param	days	几天
+@return	string	几天前的日期字符串
+ */
+func BeforeDay(days int) string {
+	now := time.Now()
+	return FormatDate(now.AddDate(0, 0, -days), DATE_FORMAT_LAYOUT)
+}
+
+/**
+获取前日期几天后的日期
+日期模板：2006-01-02
+@param	days	几天
+@return	string	几天后的日期字符串
+ */
+func AfterDay(days int) string {
+	now := time.Now()
+	return FormatDate(now.AddDate(0, 0, days), DATE_FORMAT_LAYOUT)
 }
--- dateutil/dateutil_test.go
+++ dateutil/dateutil_test.go
@@ -26,23 +26,45 @@ func TestGetDateTime(t *testing.T) {
 }
 
 func TestParseTime(t *testing.T) {
-	var time1="11:45:21"
-	tm:=ParseTime(time1)
+	var time1 = "11:45:21"
+	tm := ParseTime(time1)
 	fmt.Println(tm)
 	//output:0000-01-01 11:45:21 +0800 CST
 
-	time1="2016-08-21"
-	tm=ParseDate(time1)
+	time1 = "2016-08-21"
+	tm = ParseDate(time1)
 	fmt.Println(tm)
 	//output:2016-08-21 00:00:00 +0800 CST
 
-	time1="2016-08-21 11:45:21"
-	tm=ParseDateTime(time1)
+	time1 = "2016-08-21 11:45:21"
+	tm = ParseDateTime(time1)
 	fmt.Println(tm)
 	//output:2016-08-21 11:45:21 +0800 CST
 
-	time1="2016/8/21 1:45:23"
-	tm=ParseByLayout(time1,"2006/1/02 15:04:05")
+	time1 = "2016/8/21 1:45:23"
+	tm = ParseByLayout(time1, "2006/1/02 15:04:05")
 	fmt.Println(tm)
 	//output:2016-08-21 01:45:23 +0800 CST
+}
+
+func TestBeforeDay(t *testing.T) {
+	//current day 2016-08-22
+	fmt.Println(BeforeDay(23))
+	//2016-07-30
+}
+
+func TestAfterDay(t *testing.T) {
+	//current day 2016-08-22
+	fmt.Println(AfterDay(1))
+	//output:2016-08-23
+}
+
+func TestFormatConvert(t *testing.T) {
+	fmt.Println(FormatConvert("2016-08-23", DATE_FORMAT_LAYOUT, DATE_FORMAT_LAYOUT2))
+	fmt.Println(FormatConvert("2016-08-23", DATE_FORMAT_LAYOUT, DATE_FORMAT_LAYOUT_CN))
+	fmt.Println(FormatConvert("2016-08-23", DATE_FORMAT_LAYOUT, DATE_FORMAT_LAYOUT1))
+	//output:
+	//2016/08/23
+	//2016年8月23日
+	//20160823
 }
