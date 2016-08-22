041350c404d39cba7aaf05dc99f14bbf62e25fb7
wenit <435488988@qq.com>
2016-08-22 13:05:09 +0800

    添加日期工具类

--- dateutil/dateutil.go
+++ dateutil/dateutil.go
@@ -1,11 +1,19 @@
 package dateutil
 
-import "time"
+import (
+	"time"
+	"fmt"
+)
 
 const DATE_FORMAT_LAYOUT  = "2006-01-02"
 const TIME_FORMAT_LAYOUT  = "15:04:05"
 const DATE_TIME_FORMAT_LAYOUT  = "2006-01-02 15:04:05"
 
+/**
+获取当前日期
+转换模板：2006-01-02
+return	string
+ */
 func GetDate() string {
 	timestamp:=time.Now().Unix()
 	tm:=time.Unix(timestamp,0)
@@ -13,6 +21,11 @@ func GetDate() string {
 	return _date
 }
 
+/**
+获取当前时间
+转换模板：15:04:05
+return	string
+ */
 func GetTime() string {
 	timestamp:=time.Now().Unix()
 	tm:=time.Unix(timestamp,0)
@@ -20,9 +33,69 @@ func GetTime() string {
 	return _date
 }
 
+/**
+获取当前日期+时间
+转换模板：2006-01-02 15:04:05
+return	string
+ */
 func GetDateTime() string {
 	timestamp:=time.Now().Unix()
 	tm:=time.Unix(timestamp,0)
 	_date:=tm.Format(DATE_TIME_FORMAT_LAYOUT)
 	return _date
+}
+
+/**
+将日期字符串转换成time类型
+转换模板："2006-01-02"
+@param	value	待转换的时间字符串
+@return	Time	时间类型
+ */
+func ParseDate(value string) time.Time {
+	loc, _ := time.LoadLocation("Local")
+	t,err:=time.ParseInLocation(DATE_FORMAT_LAYOUT,value,loc)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return t
+}
+
+/**
+将时间字符串转换成time类型
+转换模板："15:04:05"
+@param	value	待转换的时间字符串
+@return	Time	时间类型
+ */
+func ParseTime(value string) time.Time {
+	loc, _ := time.LoadLocation("Local")
+	t,err:=time.ParseInLocation(TIME_FORMAT_LAYOUT,value,loc)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return t
+}
+
+/**
+将日期时间字符串转换成time类型
+转换模板：2006-01-02 15:04:05
+@param	value	待转换的时间字符串
+@return	Time	时间类型
+ */
+func ParseDateTime(value string) time.Time {
+	return ParseByLayout(value,DATE_TIME_FORMAT_LAYOUT)
+}
+
+/**
+根据指定类型的模板转换时间字符串为时间类型
+@param	value	待转换的时间字符串
+@param	layout	转换模板
+@return	Time	时间类型
+ */
+func ParseByLayout(value string ,layout string) time.Time {
+	loc, _ := time.LoadLocation("Local")
+	t,err:=time.ParseInLocation(layout,value,loc)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return t
 }
--- dateutil/dateutil_test.go
+++ dateutil/dateutil_test.go
@@ -16,3 +16,25 @@ func TestGetTime(t *testing.T) {
 func TestGetDateTime(t *testing.T) {
 	fmt.Println(GetDateTime())
 }
+
+func TestParseTime(t *testing.T) {
+	var time1="11:45:21"
+	tm:=ParseTime(time1)
+	//output:0000-01-01 11:45:21 +0800 CST
+	fmt.Println(tm)
+
+	time1="2016-08-21"
+	tm=ParseDate(time1)
+	//output:2016-08-21 00:00:00 +0800 CST
+	fmt.Println(tm)
+
+	time1="2016-08-21 11:45:21"
+	tm=ParseDateTime(time1)
+	//output:2016-08-21 11:45:21 +0800 CST
+	fmt.Println(tm)
+
+	time1="2016/8/21 1:45:23"
+	tm=ParseByLayout(time1,"2006/1/02 15:04:05")
+	//output:2016-08-21 01:45:23 +0800 CST
+	fmt.Println(tm)
+}
